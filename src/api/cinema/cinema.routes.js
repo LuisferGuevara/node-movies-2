595d547789278952123893a60c048a36bddf884e
Neftali Cruz <110734357+NeftaliCruzSosa@users.noreply.github.com>
2022-10-26 00:47:20 +0200

    FEAT: Añadido metodo post de cinema

--- src/api/cinema/cinema.routes.js
+++ src/api/cinema/cinema.routes.js
@@ -10,6 +10,20 @@ router.get('/', async(req, res) => {
     } catch (error) {
         return res.status(500).json(error);
     }
-})
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        const newCinema = new Cinema({
+            name: req.body.name,
+            location: req.body.location,
+            movies: []
+        });
+        const createdCinema = await newCinema.save();
+        return res.status(201).json(createdCinema);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
 
 module.exports = router;
